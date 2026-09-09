12eef24b3e0e4fae0e992327422faf12eda39bf7
Priyanshu Singh <priyanshusingh00031@gmail.com>
2026-09-09 12:54:07 +0530

    Fix server setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./configs/connectDb.js"
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config()
 
