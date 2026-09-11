098bc24954ca584b5219f70b14e21d9c94c5b906
Priyanshu Singh <priyanshusingh00031@gmail.com>
2026-09-11 23:20:16 +0530

    added basic pre requisites

--- server/index.js
+++ server/index.js
@@ -3,11 +3,17 @@ import dotenv from "dotenv"
 import connectDB from "./configs/connectDb.js"
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.route.js";
+import cors from "cors";
+dotenv.config();
 
-dotenv.config()
+
 
 
 const app = express()
+app.use(cors({
+    origin:"http://localhost:5173/",
+    credentials:true
+}))
 
 
 app.use(express.json())
